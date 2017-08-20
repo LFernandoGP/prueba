19315b9de8089b0a803025ac11cdce7617b89c78
fernando <fernando93d@gmail.com>
2017-08-20 21:06:58 +0200

    Logout commit

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ def function():
 def function():
 	return "log.html"
 
+@app.route("/logout")
+def logout():
+	return render_template('index.html')
+
 if __name__ == '__main__':
 	app.run(debug=True)
